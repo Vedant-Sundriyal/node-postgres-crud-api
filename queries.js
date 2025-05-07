1772581a27a4c1e938e8f8b33a4e389976e800f4
Vedant-Sundriyal <sundriyalvedant51@gmail.com>
2025-05-07 14:15:20 -0400

    FInal Commit

--- queries.js
+++ queries.js
@@ -1,11 +1,14 @@
-const Pool = require('pg').Pool;
+require('dotenv').config();
+
+const Pool = require('pg').Pool
 const pool = new Pool({
-  user: 'me',
-  host: 'localhost',
-  database: 'api2',
-  password: '12345',
-  port: 5432,
-});
+  user: process.env.DB_USERNAME,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+})
+
 const getUsers = (request, response) => {
   pool.query('SELECT * FROM users ORDER BY id ASC', (error, results) => {
     if (error) {
